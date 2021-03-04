d87870a5c9bd3868b7ae265af415ac7dc9ef3646
Jacob <jbuchanan21@qub.ac.uk>
2021-03-04 20:57:18 +0000

    Maybe fixed netlify config for reloading

---
+++ next.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // exportPathMap: async function (
+    //   defaultPathMap,
+    //   { dev, dir, outDir, distDir, buildId }
+    // ) {
+    //   return {
+    //     '/': { page: '/' },
+    //     '/campaigns': { page: '/campaigns' },  
+    //   }
+    // },
+    // async rewrites() {
+    //     return [
+    //       {
+    //         source: '/:any*',
+    //         destination: '/',
+    //       },
+    //     ];
+    //   },
+  }
+  
